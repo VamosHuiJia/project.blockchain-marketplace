8301c882f58390a532f53052cf80caac332f1928
VamosHuiJia <giahuy20092004@gmail.com>
2025-10-21 23:27:25 +0700

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col md:flex-row w-4/5 justify-between items-center mx-auto py-10'>
+        <div>
+            <div>
+                <h1>Trang trao đổi $ mua bán <br />
+                Các tác phẩm nghệ thuật ,<br />
+                Giao dịch bằng <span>NFT</span> 
+                </h1>
+                <p>Trao đổi và sở hữu cho mình những NFT đang gây sốt nhất!</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
