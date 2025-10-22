ce1fecd4202e4ef5da449ec2de37651beab8ea39
VamosHuiJia <giahuy20092004@gmail.com>
2025-10-22 09:27:14 +0700

    chinh sua

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,18 @@ import Headerlogo from '../assets/H.Q.N_logo.png'
 
 const Header = () => {
   return (
-    <div className='w-4/5 flex justify-between md:justify-center items-center py-4 mx-auto'>
+    <div className='w-4/5 flex justify-between md:justify-center items-center py-2 mx-auto'>
         {/* Logo */}
         <div className='md:flex-[0.5] flex-initial justify-center items-center'>
-            <img className='w-40 cursor-pointer' src={Headerlogo} alt="logo" />
+            <img className='w-45 cursor-pointer' src={Headerlogo} alt="logo" />
         </div>
 
         {/* Danh mục */}
-        <ul className='md:flex-[0.5] md:flex text-white hidden list-none justify-between items-center flex-initial'>
+        <ul className='md:flex-[0.5] md:flex text-white hidden list-none justify-around items-center flex-initial'>
             <li className='mx-2 cursor-pointer'>Sản phẩm</li>
             <li className='mx-2 cursor-pointer'>Tác giả</li>
             <li className='mx-2 cursor-pointer'>Tính năng</li>
-            <li className='mx-4 cursor-pointer'>Cộng đồng</li>
+            <li className='mx-2 cursor-pointer'>Cộng đồng</li>
         </ul>
 
         <button className='shadow-xl shadow-black text-white bg-[#e32970] hover:bg-[#bd255f] md:text-xl p-2 ml-4 rounded-full'>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,65 @@
 import React from 'react'
+import HeroArt from '../assets/HeroArt.jpg'
 
 const Hero = () => {
   return (
     <div className='flex flex-col md:flex-row w-4/5 justify-between items-center mx-auto py-10'>
-        <div>
+        <div className='md:w-3/6 w-full'>
             <div>
-                <h1>Trang trao đổi $ mua bán <br />
-                Các tác phẩm nghệ thuật ,<br />
-                Giao dịch bằng <span>NFT</span> 
-                </h1>
-                <p>Trao đổi và sở hữu cho mình những NFT đang gây sốt nhất!</p>
+              <h1 className='text-white text-4xl font-bold'>
+                Trang trao đổi & mua bán 
+                các tác phẩm nghệ thuật 
+              </h1>
+
+              <h2 className='text-white text-4xl mt-5 font-bold text-gradient'>
+                Giao dịch bằng NFT 
+              </h2>
+
+              <p className='text-gray-300 font-semibold text-sm mt-3'>
+                Trao đổi và sở hữu cho mình những NFT đang gây sốt nhất!</p>
+            </div>
+
+            <div>
+              <button className='shadow-xl shadow-black text-white bg-[#e32970] hover:bg-[#bd255f] font-semibold rounded-full cursor-pointer py-3 px-5 mt-3'>
+                Tạo NFT
+              </button>
+            </div>
+
+            <div className='w-3/4 flex justify-between items-center my-5'>
+              <div>
+                <p className='text-white font-bold'>123k</p>
+                <small className='text-gray-300'>lượt truy cập</small>
+              </div>
+
+              <div>
+                <p className='text-white font-bold'>400</p>
+                <small className='text-gray-300'>tác phẩm</small>
+              </div>
+
+              <div>
+                <p className='text-white font-bold'>57</p>
+                <small className='text-gray-300'>tác giả</small>
+              </div>
             </div>
         </div>
+
+        <div className="shadow-xl shadow-black md:w-2/5 w-full mt-10 md:mt-0 rounded-md overflow-hidden bg-gray-800">
+          <img
+            src={HeroArt}
+            alt='Hero'
+            className="h-60 w-full object-cover"
+          />
+        <div className="flex justify-start items-center p-3">
+          
+          <div>
+            <p className="text-white font-semibold">
+              Kết nối tới tài khoản của bạn
+            </p>
+            <small className="text-pink-800 font-bold">@you</small>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
