3da6b4a6795fb71163acfa5664316ba37ba1b7c9
VamosHuiJia <giahuy20092004@gmail.com>
2025-11-07 11:04:51 +0700

    fix lai

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HeroArt from '../assets/HeroArt.jpg'
+import Author1 from '../assets/Vincent_van_Gogh.jpg'
 import { setGlobalState } from '../store'
 
 const Author = () => {
@@ -31,28 +31,16 @@ const Card = ({ nft }) => {
   return (
     <div className='w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3'>
       <img className='h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3'
-        src={HeroArt} alt="NFT Image" />
+        src={Author1} alt="NFT Image" />
 
       <h4 className='text-white font-semibold'>
-        NFT #{nft}
+        Tác giả #{nft}
       </h4>
 
-      <p className='text-gray-400 text-sm my-1'>
+      <p className='text-gray-400 text-sm my-1 h-7'>
         Xin chào Việt Nam
       </p>
 
-      <div className='flex justify-between items-center mt-3 text-white'>
-        <div className="flex flex-col">
-          <small className='text-xs'>Giá tiền</small>
-          <p className='text-sm font-semibold'>0.55 ETH</p>
-        </div>
-
-        {/*Xem chi tiết sản phẩm*/}
-        <button  className="shadow-lg shadow-black text-white text-sm bg-[#e32970] hover:bg-[#bd255f] cursor-pointer rounded-full py-2 px-3"
-            onClick={() => setGlobalState('showModal', 'scale-100')}>
-          Xem chi tiết
-        </button>
-      </div>
 
     </div>
   )
